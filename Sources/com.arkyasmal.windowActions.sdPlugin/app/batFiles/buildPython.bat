b45f6486a301d270f111df288d58973cf870ca21
Arky Asmal <arkyasmal@gmail.com>
2023-10-04 15:49:57 -0400

    fixed build issue with pyinput

--- Sources/com.arkyasmal.windowActions.sdPlugin/app/batFiles/buildPython.bat
+++ Sources/com.arkyasmal.windowActions.sdPlugin/app/batFiles/buildPython.bat
@@ -4,7 +4,8 @@ pip install -r requirements.txt
 @REM pyinstaller pluginActions.py -F -n pluginActions --distpath ../executables --workpath ../executables --specpath ../executables --add-data ../dll:.
 pyinstaller connection.py -F -n run --distpath ../../ --workpath ../../ --specpath ../../ --add-data ./app/dll:.
 @REM rmdir /s /q ..\executables\pluginActions
+rmdir /s /q ..\..\run
 @REM del ..\pluginActions.spec
-del ..\run.spec
+del ..\..\run.spec
 cd ..
 cd ..
