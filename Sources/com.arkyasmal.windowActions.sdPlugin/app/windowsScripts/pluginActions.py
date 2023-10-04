e2d522522011660b911b86f56c9b3510db90868c
Arky Asmal <arkyasmal@gmail.com>
2023-10-04 04:55:58 -0400

    add release file and fix tuple unpacking bug

--- Sources/com.arkyasmal.windowActions.sdPlugin/app/windowsScripts/pluginActions.py
+++ Sources/com.arkyasmal.windowActions.sdPlugin/app/windowsScripts/pluginActions.py
@@ -14,7 +14,7 @@ if __name__ == "__main__":
         case 'move_window':
             desktop_num = cmd_args[cmd_args.index("--newDesktop") + 1]
             desktop_num = one_indexed(desktop_num)
-            win_id, win_id_type = get_window_id(cmd_args)
+            (win_id, win_id_type) = get_window_id(cmd_args)
             move_windows_to_new_desktop(desktop_num, win_id_type, win_id)
         case 'move_virtual_desktop': 
             desktop_num = cmd_args[cmd_args.index("--newDesktop") + 1]
@@ -23,7 +23,7 @@ if __name__ == "__main__":
         case 'move_window_to_monitor': 
             monitor_num = cmd_args[cmd_args.index("--newMonitor") + 1]
             monitor_num = one_indexed(monitor_num)
-            win_id, win_id_type = get_window_id(cmd_args)
+            (win_id, win_id_type) = get_window_id(cmd_args)
             move_windows_to_new_monitor(monitor_num, win_id_type, win_id)
         case 'get_monitor_info':
             app_data_directory = cmd_args[cmd_args.index("--appDataDirectory") + 1]
@@ -32,16 +32,16 @@ if __name__ == "__main__":
             direction = cmd_args[cmd_args.index("--direction") + 1]
             toggle_through_virtual_desktops(int(direction))
         case 'minimize_window':
-            win_id, win_id_type = get_window_id(cmd_args)
+            (win_id, win_id_type) = get_window_id(cmd_args)
             minimize_window(win_id_type, win_id)
         case 'maximize_window':
-            win_id, win_id_type = get_window_id(cmd_args)
+            (win_id, win_id_type) = get_window_id(cmd_args)
             maximize_window(win_id_type, win_id)
         case 'close_window': 
-            win_id, win_id_type = get_window_id(cmd_args)
+            (win_id, win_id_type) = get_window_id(cmd_args)
             close_window(win_id_type, win_id)
         case 'resize_window':
-            win_id, win_id_type = get_window_id(cmd_args)
+            (win_id, win_id_type) = get_window_id(cmd_args)
             size = cmd_args[cmd_args.index("--size") + 1].strip('][').split(',')
             coordinates = cmd_args[cmd_args.index("--coordinates") + 1].strip('][').split(',')
             resize_window(win_id_type, win_id, size, coordinates)
