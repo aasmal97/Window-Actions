80f0ba384bcdbdcd3790e3d53c507fd0e006e6e7
Arky Asmal <arkyasmal@gmail.com>
2023-12-09 03:47:54 -0500

    fixed get_active_window error caused by providing improper parameter

--- Sources/com.arkyasmal.windowActions.sdPlugin/app/windowsScripts/getMatchingWindowList.py
+++ Sources/com.arkyasmal.windowActions.sdPlugin/app/windowsScripts/getMatchingWindowList.py
@@ -6,8 +6,7 @@ def test_regex(pattern, testStr):
 def get_matching_windows_list(win_id_type, win_id):
     id_type = "title" if win_id_type == 'win_title' or win_id_type == 'win_ititle' else win_id_type
     is_partial_str = win_id_type == 'win_ititle'
-    file_path = r"Elgato\StreamDeck\Plugins\com.arkyasmal.windowActions.txt"
-    all_windows = get_active_windows(app_data_directory = file_path)
+    all_windows = get_active_windows()
     matching_windows_itr = filter(lambda window: test_regex(win_id, window[id_type]) if is_partial_str else window[id_type] == win_id, all_windows)
     matching_windows = list(matching_windows_itr)
     return matching_windows
