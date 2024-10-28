5d59c8854ac5672c7fdb795353039b0cc332b41d
Arky <arkyasmal@gmail.com>
2024-10-28 17:29:18 -0400

    updated package installation

--- Sources/com.arkyasmal.windowActions.sdPlugin/app/windowsScripts/determineActiveWindows.py
+++ Sources/com.arkyasmal.windowActions.sdPlugin/app/windowsScripts/determineActiveWindows.py
@@ -1,4 +1,4 @@
-#pywintypes import is required or else .exe won't build properly
+# pywintypes import is required or else .exe won't build properly
 import pywintypes
 import win32gui
 import win32process
@@ -8,18 +8,24 @@ import csv
 import ctypes
 from pathlib import Path
 enumWindows = ctypes.windll.user32.EnumWindows
-enumWindowsProc = ctypes.WINFUNCTYPE(ctypes.c_bool, ctypes.c_int, ctypes.POINTER(ctypes.c_int))
+enumWindowsProc = ctypes.WINFUNCTYPE(
+    ctypes.c_bool, ctypes.c_int, ctypes.POINTER(ctypes.c_int))
 isWindowVisible = ctypes.windll.user32.IsWindowVisible
+
+
 def get_window_info(hwnd):
     window_text = win32gui.GetWindowText(hwnd)
     if window_text:
         return {'_hWnd': hwnd, 'title': window_text}
     else:
         return None
-def get_all_windows(): 
+
+
+def get_all_windows():
     """Returns a list of Window objects for all visible windows.
     """
     windowObjs = []
+
     def foreach_window(hWnd, lParam):
         if isWindowVisible(hWnd) != 0:
             windowInfo = get_window_info(hWnd)
@@ -27,42 +33,50 @@ def get_all_windows():
                 windowObjs.append(windowInfo)
     enumWindows(enumWindowsProc(foreach_window), 0)
     return windowObjs
-def get_all_process(): 
-    all_processes = os.popen("wmic process get name, processid /format:csv").read()
+
+
+def get_all_process():
+    all_processes = os.popen(
+        "wmic process get name, processid /format:csv").read()
     all_processes_split = all_processes.split("\n")
-    all_processes_split = list(filter(lambda x: x !='', all_processes_split))
+    all_processes_split = list(filter(lambda x: x != '', all_processes_split))
     reader = csv.DictReader(all_processes_split, delimiter=',')
     all_processes_list = list(reader)
     return all_processes_list
-def get_window_class_names(active_win_data, filter_dup=False): 
-    win_class_names = [{**x, "win_class": win32gui.GetClassName(x["hWnd"])} for x in active_win_data]
+
+
+def get_window_class_names(active_win_data, filter_dup=False):
+    win_class_names = [
+        {**x, "win_class": win32gui.GetClassName(x["hWnd"])} for x in active_win_data]
     new_map = {}
     new_data = []
-    if filter_dup: 
+    if filter_dup:
         for x in win_class_names:
             win_class = x["win_class"]
             if not win_class in new_map:
                 new_data.append(x)
                 new_map[win_class] = True
-    else: 
+    else:
         new_data = win_class_names
     return new_data
+
+
 def get_active_windows(
-        filter_dup = False
-    ):
+    filter_dup=False
+):
     all_process = get_all_process()
-    #generate map using PID as key
+    # generate map using PID as key
     process_map = {}
-    for x in all_process: 
+    for x in all_process:
         pid = x["ProcessId"]
         process_map[pid] = x
     # get all active windows
     windows = get_all_windows()
-    windows_data=[
+    windows_data = [
         {
-            "hWnd": x["_hWnd"], "title": x['title'], 
+            "hWnd": x["_hWnd"], "title": x['title'],
             "pid": win32process.GetWindowThreadProcessId(x["_hWnd"])
-        } 
+        }
         for x in windows
     ]
     new_data = list(filter(lambda x: len(x["title"]) > 0, windows_data))
